7f4a6f064af2ee41ceb611bde0a1bd8ec642061c
Pedro Azevedo <75546030+AshbornXS@users.noreply.github.com>
2024-11-17 19:47:55 -0300

    changed from single repo to separate repos - added more functions to the user profile - added favorite system

--- backend/src/main/java/org/registry/akashic/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.registry.akashic.repository;
-
-import org.registry.akashic.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-}
